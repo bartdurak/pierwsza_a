069c367ff6c76b565c0d4000423ead2578a363c0
bartdurak <batq.ar@zoho.com>
2022-11-28 01:50:46 +0100

    podaj imie

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# py_2 For 1a programist ZST
+
+imie= input("podaj imie: ")
+print("czesc " + imie)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
